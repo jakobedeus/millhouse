86e7978f2baf1429a937978018df0a6ccd7b16c7
jakobedeusb <jakob@edeus.segit config --global user.email jakob@edeus.segit config --global user.email>
2018-11-26 16:19:02 +0100

    Add everything

---
+++ views/feed.php
@@ -0,0 +1,29 @@
+<?php include "../includes/head-views.php" ?>
+
+    <main class="container">
+    
+        <section>
+
+            <article class="feed_post row">
+                <div class="col-8">
+                    <h2 class="post_title"></h2>
+                    <p class="date"></p>
+                    <p class="category"></p>
+                    <p class="content"></p>
+                    <p class="number_of_comments"></p>
+                    <a href=""><p class="comments"></p></a>
+                </div>
+                <div class="img_container col-4">
+                    <img src="" alt="">
+                </div>
+            </article>
+
+        </section>
+
+        <section class="pagination col-12">
+            <a href=""><p class="comments"></p></a>
+        </section>
+    </main>
+    
+</body>
+</html>
---
+++ views/post.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Feed</title>
+</head>
+<body>
+
+    <main class="container">
+    
+        <section>
+
+            <article class="feed_post row">
+                <div class="col-8">
+                    <h2 class="post_title"></h2>
+                    <p class="date"></p>
+                    <p class="category"></p>
+                    <p class="content"></p>
+                    <p class="number_of_comments"></p>
+                    <a href=""><p class="comments"></p></a>
+                </div>
+                <div class="img_container col-4">
+                    <img src="" alt="">
+                </div>
+            </article>
+
+        </section>
+
+        <form action="" class="form_comment">
+            <textarea name="" id=""></textarea>
+            <input type="submit">
+        </form>
+    </main>
+    
+</body>
+</html>
