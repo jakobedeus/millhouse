ffbb33ab30e85ca6233b2d45681b4ed2fc061106
ronjamyhr <ronja_myhr@hotmail.com>
2018-11-28 17:35:14 +0100

    Add pdo and fetch to the class Posts

--- includes/fetch_posts.php
+++ includes/fetch_posts.php
@@ -1,6 +1,7 @@
-<?php
+<?php/*
 include 'database-connection.php';
 
 $fetch_all_images_statement = $pdo->prepare("SELECT * FROM posts");
 $fetch_all_images_statement->execute();
 $all_posts = $fetch_all_images_statement->fetchAll(PDO::FETCH_ASSOC);
+*/?>
