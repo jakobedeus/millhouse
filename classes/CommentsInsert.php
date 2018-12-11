332d42a60950d60123e74d6eb829f8e7372178f3
Olivia Lillemor Hansson Lindblom <Lolla@Olivias-MBP-2.mediegruppen.se>
2018-12-11 16:15:21 +0100

    cleaning code

--- classes/CommentsInsert.php
+++ classes/CommentsInsert.php
@@ -1,4 +1,5 @@
 <?php
+
 $pdo = new PDO(
    "mysql:host=localhost;dbname=millhouse;charset=utf8",
    "root", //user
@@ -8,35 +9,35 @@ $pdo = new PDO(
 class CommentsInsert
 {
 
- private $pdo;
+    private $pdo;
 
- public function __construct($pdo)
- {
-   $this->pdo = $pdo;
- }
+    public function __construct($pdo)
+    {
+      $this->pdo = $pdo;
+    }
 
+    public function insertComments()
+    {
+      //Redefine varibles to make them more understandable and easier to user
+      $created_by = $_SESSION["user_id"];
+      $content = $_POST["content"];
+      $post_id_insert = $_POST["comment_post_id"];
+      date_default_timezone_set("Europe/Stockholm");
+      $datetime = date('Y/m/d H:i');    
 
- public function insertComments()
- {
-  // Redefine varibles to make them more understandable and easier to user
-   $created_by = $_SESSION["user_id"];
-   $content = $_POST["content"];
-   $post_id_insert = $_POST["comment_post_id"];
-   date_default_timezone_set("Europe/Stockholm");
-   $datetime = date('Y/m/d H:i');
-
- $statement_insert_comment = $this->pdo->prepare("INSERT INTO comments
- (content, post_id, created_by, date) VALUES (:content, :post_id, :created_by, :date)");
- $statement_insert_comment->execute(
- [
-   ":content" => $content,
-   ":post_id" => $post_id_insert,
-   ":created_by" => $created_by,
-   ":date" => $datetime
- ]
- );
- $insert_comment = $statement_insert_comment;
- return $insert_comment;
-}
+      $statement_insert_comment = $this->pdo->prepare("INSERT INTO comments
+      (content, post_id, created_by, date) VALUES (:content, :post_id, :created_by, :date)");
+      $statement_insert_comment->execute(
+        [
+          ":content" => $content,
+          ":post_id" => $post_id_insert,
+          ":created_by" => $created_by,
+          ":date" => $datetime
+        ]
+      );
+      
+    $insert_comment = $statement_insert_comment;
+    return $insert_comment;
+    }
 }
 
