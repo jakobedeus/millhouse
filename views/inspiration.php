ee360d3742adcbd7d68487a4145fbce87c1ea43c
Revasser <parmis.kamali@gmail.com>
2018-12-11 15:30:40 +0100

    add styling to parallax

--- views/inspiration.php
+++ views/inspiration.php
@@ -1,34 +1,26 @@
 <?php
 include "../includes/head-views.php";
-
-
 ?>
 
-
-
 <main>
-
-    <div class="parallax">
+  <div class="parallax">
     <div class="white_overlay">
+      <h1>LUXURY IS IN EACH DETAIL.</h1>
     </div>
-    </div>
-
+  </div>
     <div class="parallax_2">
       <div class="white_overlay_2">
-        <a href="http://localhost:8888/millhouse/views/feed.php?category=living">LIVING</a>
+        <p class="parallax_paragraph"><a href="http://localhost:8888/millhouse/views/feed.php?category=living">LIVING.</a></p>
       </div>
     </div>
-
     <div class="parallax_3">
-      <div class="white_overlay_3">
-        <a href="http://localhost:8888/millhouse/views/feed.php?category=watches"></a>
+      <div class="white_overlay_2">
+        <p class="parallax_paragraph"><a href="http://localhost:8888/millhouse/views/feed.php?category=watches">WATCHES.</a></p>
       </div>
     </div>
     <div class="parallax_4">
-      <div class="white_overlay_4">
-        <a href="http://localhost:8888/millhouse/views/feed.php?category=sunglasses">SUNGLASSES</a>
+      <div class="white_overlay_2">
+        <p class="parallax_paragraph"><a href="http://localhost:8888/millhouse/views/feed.php?category=sunglasses">SUNGLASSES.</a></p>
       </div>
     </div>
-
-
 </main>
