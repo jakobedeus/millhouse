3ff8a29dbf0dc232fcad7d674745d1de8d99e972
jakobedeusb <jakob@edeus.segit config --global user.email jakob@edeus.segit config --global user.email>
2018-12-17 17:02:22 +0100

    add correct database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 17, 2018 at 03:58 PM
+-- Generation Time: Dec 17, 2018 at 04:01 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -13,6 +13,59 @@ SET time_zone = "+00:00";
 --
 -- Database: `millhouse`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`) VALUES
+(1, 'living'),
+(2, 'sunglasses'),
+(3, 'watches');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `content`, `post_id`, `created_by`, `date`) VALUES
+(124, 'This vase would be the perfect gift for my wife!', 5, 6, '2018/12/14 14:44'),
+(125, 'Oh my god! That color is A M A Z I N G', 5, 7, '2018/12/14 14:45'),
+(126, ' Looking goooooooood.', 6, 8, '2018/12/14 14:47'),
+(127, 'A perfect Christmas gift!', 6, 9, '2018/12/14 14:48'),
+(128, 'I would love to have that globe at home...', 7, 10, '2018/12/14 14:51'),
+(129, 'Oh wow. I need to buy all that!!', 7, 11, '2018/12/14 14:51'),
+(130, 'cool shades man!', 9, 12, '2018/12/14 15:22'),
+(131, 'Damn they’re fine', 9, 13, '2018/12/14 15:23'),
+(132, 'Like the model! :D do they come in more colors?', 9, 14, '2018/12/14 15:24'),
+(133, 'I need this watch now, when can buy it?', 11, 3, '2018/12/14 15:28'),
+(134, 'I agree with James, will it be available in calf leather?', 11, 4, '2018/12/14 15:29'),
+(135, 'This watch is amazing, cant wait to purchase it.', 11, 5, '2018/12/14 15:29'),
+(136, 'Love these! Would love to have them in silver! :-)', 13, 15, '2018/12/14 15:42');
 
 -- --------------------------------------------------------
 
@@ -42,9 +95,54 @@ INSERT INTO `posts` (`id`, `title`, `date`, `image`, `content`, `created_by`, `i
 (11, 'WATCH YO SELF', '2018/12/14 15:27', '../images/uploads/watch_post.jpeg', 'With a stylish, eye-catching dial and classic black leather strap, Classic Black Sheffield is an elegant watch that exudes self-confidence and adds attitude to your style. The black and crocodile patterned Classic Reading band is made of genuine Italian leather. Classic Cuff has been designed to reflect the magic beauty of our minimalist watches. Classic Cuffs relaxed yet tasteful style is a tribute to the perfect craftsmanship that lies behind all timeless products from Daniel Wellington.', 1, 3),
 (13, 'New launch for spring 19!', '2018/12/14 15:41', '../images/uploads/nya_in_action_11_14.jpg', 'We’re launching these edgy and colourful shades for spring 19’. The ‘Edie’ shades have a bit of old school with a modern twist, clean but never boring! As for now we’re launching them in red, yellow and silver. Which one is your personal favourite?', 1, 2);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(400) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `admin` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `mail`, `admin`) VALUES
+(1, 'admin', '$2y$10$XJ/.ybZBsFkPTkRAx/SxFe9uDFiNUBrLhq446jVbtoD7HSqMgVxH6', 'admin@neverends.com', 'is_admin'),
+(3, 'Declan', '$2y$10$a.Gt2kS8w9f6hlg/2ZzL6OpFwojJon1NQePld9fDDuYOEKeFdXQZK', 'hej@declan.se', 'not_admin'),
+(4, 'Eliza78', '$2y$10$OmbZTyJu8J7jkbSU6nh4SORImLJrtCpPrnx7/3bWVq8TwsEbxR4cy', 'hej@hej.hej', 'not_admin'),
+(5, 'Harvey', '$2y$10$HvtWlp/e8nzstYfVs1YYA.AaSOFgpIewRCCf/fIfd.vnXz6yOAje.', 'hej@hej.hej', 'not_admin'),
+(6, 'Tony', '$2y$10$4oMnq45QR9zJUEldqTNX8eVm08skmcVmYqozHU6CsX91egRk8HCrC', 'hej@hej.se', 'not_admin'),
+(7, 'Sofia_Larsson', '$2y$10$tsX3y5FvzTIBd8WhEpBs6uMKH4mozMjFIMcMXm2jzlU83hlQx/Uma', 'hej', 'not_admin'),
+(8, 'Maddison_hopkins', '$2y$10$rPgaPfYiMAs5YNB41s.T4O4quU6FtAfwi5ZENsw0bMMONRn/JQ7ce', 'hej', 'not_admin'),
+(9, 'Melanie_queen', '$2y$10$lwICrgpwTVv5H52deYYU3eGif8hGz5SGlOxdvReALFekt8Oi8OOji', 'hej', 'not_admin'),
+(10, 'PabloPear', '$2y$10$7zpfGWIyVowOaLv65JZGAeo5ZO78QMf/oikYWYEkrOSvntJv8YBMS', 'hej', 'not_admin'),
+(11, 'Sarah_MacGyver', '$2y$10$9xoQBB8pvpm./WNTcQ9gYedr5cCkWrxETcNvrOhOYW1z0VJLj.gBW', 'hej', 'not_admin'),
+(12, 'Joe_N_Driscoll', '$2y$10$zbMtj0PiFy.ZeKHFjOMC9uIVpkjbOceB3A1T8vrjBqYRWKYajgWgG', 'hej', 'not_admin'),
+(13, 'Kenneth_rodgers', '$2y$10$n9BaBrazpRhtPc8py4.Vk.RquZxSl28R5V5G6tjdgQtJbPuTUk1t6', 'hej', 'not_admin'),
+(14, 'SabrinaL', '$2y$10$a0EauSd71UdGYQkbQ8bmIeA8zd9MR0weiVUbobpRNUi.6aHA4KNAK', 'hej', 'not_admin');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Indexes for table `posts`
@@ -53,11 +151,35 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
